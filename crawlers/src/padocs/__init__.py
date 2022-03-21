d42371ff154da083038f9acd29bb8c2bca533e58
rhiaro <amy@rhiaro.co.uk>
2022-03-21 15:28:14 +0000

    bournemouth: add aleph stage

--- crawlers/src/padocs/__init__.py
+++ crawlers/src/padocs/__init__.py
@@ -16,12 +16,12 @@ def seed_url(context, data):
 
     db = dataset.connect(os.environ.get('EPDS_DB_URL'))
     query = """SELECT url FROM planit_key_fields
-    WHERE area_name = '%s' and start_date>='%s' and start_date<='%s' LIMIT 10""" % (area, start, end)
+    WHERE area_name = '%s' and start_date>='%s' and start_date<='%s'""" % (area, start, end)
     result = db.query(query)
     for r in result:
         url = '%s&tab=2' % r['url'] # sets to 'Associated Documents' tab
         context.emit(data={'url': url})
 
 
-def test(context, data):
+def dump(context, data):
     print(data)
