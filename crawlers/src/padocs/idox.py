17020c592992f2e7bcfc4aa29c0dbbf550354024
rhiaro <amy@rhiaro.co.uk>
2022-04-07 21:41:57 +0100

    idox: configure table and filter out drawings etc

--- crawlers/src/padocs/idox.py
+++ crawlers/src/padocs/idox.py
@@ -2,6 +2,9 @@ from urllib.parse import urljoin
 from memorious.helpers.key import make_id
 
 
+EXCLUDE_DOCS = ['plan', 'drawing', 'elevations']
+
+
 def docs_url(context, data):
     # sets to 'Documents' tab
     url = data['url'].replace('activeTab=summary', 'activeTab=documents')
@@ -9,6 +12,10 @@ def docs_url(context, data):
 
 
 def parse(context, data):
+
+    link_i = context.params.get('link_index', 5)
+    desc_i = context.params.get('description_index', 4)
+
     with context.http.rehash(data) as result:
         if result.html is not None:
             docstable = result.html.find(".//table[@id='Documents']")
@@ -16,11 +23,13 @@ def parse(context, data):
             if docstable is not None:
                 for row in docstable.findall(".//tr"):
                     tds = row.findall(".//td")
-                    if len(tds) > 0:
-                        # 0: checkbox, 1: date published, 2: document type, 3: measure, 4: description, 5: view link
-                        try:
-                            file = tds[5].find(".//a").get('href')
-                            title = tds[4].text
+                    if len(tds) >= link_i and len(tds) >= desc_i:
+
+                        file = tds[link_i].find(".//a").get('href')
+                        title = tds[desc_i].text or '(no title)'
+
+                        if include_doc(title):
+
                             url = urljoin(result.url, file)
                             doc = {
                                 'url': url,
@@ -31,7 +40,18 @@ def parse(context, data):
                             }
 
                             context.emit(data=doc)
-                        except IndexError(e):
-                            context.emit_warning("Bad table structure [%s" % result.url)
+
+                        else:
+                            context.emit_warning("Skipping document: %s [%s]" % (title, result.url))
+
             else:
                 context.emit_warning("No documents table found [%s]" % result.url)
+
+
+def include_doc(title):
+
+    for term in EXCLUDE_DOCS:
+        if term in title.lower():
+            return False
+
+    return True
